bf691e2b634ce28d73d55db4ebc520b60ca0d9fa
Christine <christinekhekama99@gmail.com>
2023-09-10 23:42:07 +0300

    complete

---
+++ lib/migrations/versions/5099e626dba0_renaming_students_to_scholars.py
@@ -0,0 +1,24 @@
+"""Renaming students to scholars
+
+Revision ID: 5099e626dba0
+Revises: 791279dd0760
+Create Date: 2023-09-10 23:34:48.254664
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '5099e626dba0'
+down_revision = '791279dd0760'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
